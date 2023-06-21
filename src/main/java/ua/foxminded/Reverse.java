ec10b60a7a162d8118f2690305142735c7c22ee8
Vlad Zhezherun <vladisimissius@gmail.com>
2023-06-21 13:36:02 +0300

    implement style correction, "for" loop for reverseSentence method

--- src/main/java/ua/foxminded/Reverse.java
+++ src/main/java/ua/foxminded/Reverse.java
@@ -1,15 +1,17 @@
 package ua.foxminded;
 
-import java.util.regex.Pattern;
-
 public class Reverse {
+
+    static final String SELECT_ALL_SYMBOLS_EXCEPT_ENG_LETTERS_FILTER = "[^A-Z, ^a-z]";
+    static final String WORD_SEPARATOR = " ";
+
     public static String reverseWord(String input) {
 
-        String onlyAlphabet = input.replaceAll("[^A-Z, ^a-z]", "");
-        StringBuilder reversedWord = new StringBuilder(onlyAlphabet).reverse();
+        String inputAlphabetSymbols = input.replaceAll(SELECT_ALL_SYMBOLS_EXCEPT_ENG_LETTERS_FILTER, "");
+        StringBuilder reversedWord = new StringBuilder(inputAlphabetSymbols).reverse();
 
         for (int i = 0; i < input.length(); i++) {
-            if (Character.valueOf(input.charAt(i)).toString().matches("[^A-Z, ^a-z]")) {
+            if (Character.valueOf(input.charAt(i)).toString().matches(SELECT_ALL_SYMBOLS_EXCEPT_ENG_LETTERS_FILTER)) {
                 reversedWord.insert(i, input.charAt(i));
             }
         }
@@ -18,13 +20,12 @@ public class Reverse {
     }
 
     public static String reverseSentence(String input) {
-        String inputWords[] = input.split(" ");
-        String outputWords[] = new String[inputWords.length];
-        int i = 0;
-        for (String inputWord : inputWords) {
-            outputWords[i] = reverseWord(inputWord);
-            i++;
+        String[] inputWords = input.split(WORD_SEPARATOR);
+        String[] outputWords = new String[inputWords.length];
+
+        for (int i = 0; i < inputWords.length; i++) {
+            outputWords[i] = reverseWord(inputWords[i]);
         }
-        return String.join(" ", outputWords);
+        return String.join(WORD_SEPARATOR, outputWords);
     }
 }
